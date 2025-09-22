7101d4a3d39baaff89c0d07c68e10f24ae691e96
Keziah Dorothy <keziahdorothy14@gmail.com>
2025-09-22 18:44:14 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,23 @@
+print(list [2])
+adding the elements:
+list = [1,2,3,4]
+print("initial list:")
+print(list)
+list.insert(0,'geeks')
+print ("/n list after performing insert operation:")
+print(list)
+list = [1,2,3,4,5,6,7,8,9,10,11,12]
+print("initial list:")
+print(list)
+list.remove(5)
+list.remove(6)
+print("/n list after removal of two elements:")
+print(list)
+for I in range(1,5):
+list.remove(i)
+print("/n list after removing a range of elements:")
+print(list)
+list = [['geeks','for'],['geeks']]
+print("/n multi-dimentional list:")
+print(list)
+
